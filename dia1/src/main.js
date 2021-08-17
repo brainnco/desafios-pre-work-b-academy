276ad671aaa262839d379ce1a6fb7e4a54b9b0ee
edenpaulocruz <edenpaulo@gmail.com>
2021-08-17 10:25:36 -0300

    Desafio do dia1 resolvido

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,13 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+const app = document.querySelector('[data-js=app]')
+const link = document.querySelector('[data-js=link]')
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+link.addEventListener('click', event => {
+  app.classList.toggle('hidden');
+})
