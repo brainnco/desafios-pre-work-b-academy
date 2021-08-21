16533bd82cca6d249c6de4e8e28fa70ef50fbf37
edenpaulocruz <edenpaulo@gmail.com>
2021-08-21 02:12:47 -0300

    Corrigido o querySelector de app

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
