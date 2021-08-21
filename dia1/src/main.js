77edcbfce48425456e798d157522d6e330313dfc
edenpaulocruz <edenpaulo@gmail.com>
2021-08-21 02:26:25 -0300

    Adicionado link para alterar a visibilidade de app

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,14 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const app = document.querySelector('[data-js="app"]')
+const link = document.querySelector('[data-js="link"]')
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+link.addEventListener('click', (e) => {
+  e.preventDefault()
+  app.classList.toggle('hidden')
+})
